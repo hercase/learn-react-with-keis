945054c5c52beacac7ecfa7d24db25229f8bef00
¨camila-stefano¨ <camilavictoriastefano@gmail.com>
2024-05-13 18:55:04 -0300

    add Pokemon Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+// Pages
+import Home from './pages/Home.jsx';
+import Pokemon from './pages/Pokemon.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/pokemon', element: <Pokemon /> },
+      { path: '/plantas', element: <p>Plantas</p> },
+      { path: '/personas', element: <p>Personas</p> },
+      { path: '*', element: <p>404</p> }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
